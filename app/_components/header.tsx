033853396ddd159024f95c9e00d4c8c2dc3230fd
Sumit Kumar <sumitsuthar103@gmail.com>
2026-09-06 16:18:45 +0530

    Updated

---
+++ app/_components/header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        {/* Logo */}
+
+        {/* Menu Options */}
+
+        {/* Get Started Button */}
+        
+    </div>
+  )
+}
+
+export default Header
